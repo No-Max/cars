65db00c63861486ce6509cc41aa8daa234de6789
4ebula <4ebula@gmail.com>
2021-05-17 15:09:20 +0300

    feat: add more info buttons and interactions

--- classes/Card.js
+++ classes/Card.js
@@ -1,5 +1,5 @@
 export default class Card {
-  constructor({ brand, model, img, price, description }) {
+  constructor({ brand, model, img, price, description, color, year}) {
     const containerElement = document.createElement('div');
     containerElement.classList.add('component-card');
 
@@ -9,7 +9,56 @@ export default class Card {
       <div class="component-card-model">${model}</div>
       <div class="component-card-price">${price}</div>
       <div class="component-card-descr">${description}</div>
+      <button href="#car" class="component-card-more">Learn more..</button>
     `;
+
+    this.car = {
+      brand: brand,
+      model: model,
+      img: img,
+      price: price,
+      description: description,
+    }
+    containerElement.querySelector('button').addEventListener('click', () => {
+      const a = document.createElement('a');
+      a.href = '#car';
+      a.click();
+      const container = document.querySelector('.component-page');
+      container.innerHTML = `
+        <div class="component-page-img">
+        <img src="img/big/${img}">
+        </div>
+        <div class="component-page-info">
+        <div class="component-page-title">${brand + ' ' + model}</div>
+        <div class="component-page-price">${price}</div>
+        <div class="component-page-descr">${description}</div>
+        <table class="component-page-table">
+          <tr>
+            <td>Максимальная скорость</td>
+            <td>${Math.trunc(Math.random() * 100 + 200)}км/ч</td>
+          </tr>
+          <tr>
+            <td>Вес</td>
+            <td>${Math.trunc(Math.random() * 700 + 1000)}кг</td>
+          </tr>
+          <tr>
+            <td>Привод</td>
+            <td>полный</td>
+          </tr>
+          <tr>
+            <td>Цвет</td>
+            <td>${color}</td>
+          </tr>
+          <tr>
+            <td>Модельный год</td>
+            <td>${year ?? Math.trunc(Math.random() * 30 + 1990)}</td>
+          </tr>
+        </table>
+        </div>
+      `;
+      console.log(event.target.closest('.component-card'));
+      console.log(this.car);
+    });
 
     return containerElement;
   }
@@ -17,5 +66,5 @@ export default class Card {
     cars.forEach(car => {
       container.append(new Card(car));
     });
-  }  
+  }
 }
