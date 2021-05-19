fe81d1a9f6cc1636aa07380760134592cf1d1ef3
No-Max <malyavko.max@gmail.com>
2021-05-19 17:48:15 +0300

    update BigCard class

--- classes/BigCard.js
+++ classes/BigCard.js
@@ -1,38 +1,35 @@
 export default class BigCard {
-    constructor({ id, brand, model, img, price, description }) {
-        const container = document.createElement('div');
-        container.classList.add('component-page');
-        container.innerHTML = `
-            <div class="component-page-img">
-              <img src="img/${img}">
-            </div>
-            <div class="component-page-info">
-              <div class="component-page-title">${brand} ${model}</div>
-              <div class="component-page-price">${price}$</div>
-              <div class="component-page-descr">${description}</div>
-              <table class="component-page-table">
-                <tr>
-                  <td>Максимальная скорость</td>
-                  <td>260км/ч</td>
-                </tr>
-                <tr>
-                  <td>Вес</td>
-                  <td>1200кг</td>
-                </tr>
-                <tr>
-                  <td>Привод</td>
-                  <td>полный</td>
-                </tr>
-                <tr>
-                  <td>Цвет</td>
-                  <td>серый</td>
-                </tr>
-                <tr>
-                  <td>Модельный год</td>
-                  <td>2020</td>
-                </tr>
-              </table>
-            </div>`
-        return container;
-    }
+  constructor({ id, brand, model, bigImg, price, description, parameters }) {
+    const container = document.createElement("div");
+    container.classList.add("component-page");
+    container.innerHTML = `
+      <div class="component-page-img">
+        <img src="img/${bigImg}">
+      </div>
+      <div class="component-page-info">
+        <div class="component-page-title">${brand} ${model}</div>
+        <div class="component-page-price">${price}$</div>
+        <div class="component-page-descr">${description}</div>
+        <table class="component-page-table">
+          ${createParametersTable(parameters)}
+        </table>
+      </div>`;
+    return container;
+  }
+  static appendCard(container, car) {
+    container.innerHTML = "";
+    container.append(new BigCard(car));
+  }
+}
+
+function createParametersTable(parameters) {
+  let html = "";
+  parameters.forEach((parameter) => {
+    html += `
+      <tr>
+        <td>${parameter.name}</td>
+        <td>${parameter.value}</td>
+      </tr>`;
+  });
+  return html;
 }
