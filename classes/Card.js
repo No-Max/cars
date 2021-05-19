3aae8fc21ca981e6ee89a2926e622ffba16ca40e
No-Max <malyavko.max@gmail.com>
2021-05-19 17:47:49 +0300

    update Card container

--- classes/Card.js
+++ classes/Card.js
@@ -1,21 +1,27 @@
 export default class Card {
-  constructor({ brand, model, img, price, description }) {
-    const containerElement = document.createElement('div');
-    containerElement.classList.add('component-card');
+  constructor({ id, brand, model, img, price, description }, onClick) {
+    const containerElement = document.createElement("div");
+    containerElement.classList.add("component-card");
 
     containerElement.innerHTML = `
       <div class="component-card-img" style="background-image: url('/img/${img}')"></div>
       <div class="component-card-brand">${brand}</div>
       <div class="component-card-model">${model}</div>
       <div class="component-card-price">${price}</div>
-      <div class="component-card-descr">${description}</div>
+      <div class="component-card-descr">${description}</div>  
     `;
+    const buttonReadMore = document.createElement("button");
+    buttonReadMore.innerText = "Подробнее";
+    buttonReadMore.onclick = () => {
+      onClick(id);
+    };
+    containerElement.append(buttonReadMore);
 
     return containerElement;
   }
-  static appendCards(container, cars) {
-    cars.forEach(car => {
-      container.append(new Card(car));
+  static appendCards(container, cars, onClick) {
+    cars.forEach((car) => {
+      container.append(new Card(car, onClick));
     });
-  }  
+  }
 }
