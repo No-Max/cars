e261aab53df7b9b43345bad9e8decfa5045ed94f
MyAccJS <yarokoon@gmail.com>
2021-05-25 16:56:15 +0300

    Fetched blyat

--- classes/Dropdown.js
+++ classes/Dropdown.js
@@ -1,63 +1,70 @@
 export default class Dropdown {
-  _selectedValue = null
+    _selectedValue = null;
 
-  constructor(defaultValue, listItems, onSelectItem) {
-    this.defaultValue = defaultValue;
-    this._selectedValue = defaultValue;
-    this.listItems = listItems;
-    
-    this.element = document.createElement('div');
-    this.element.classList.add('component-dropdown-container');
-    
-    this.valueElement = document.createElement('div');
-    this.valueElement.classList.add('component-dropdown-value')
-    this.valueElement.innerText = this.defaultValue;
-    this.element.append(this.valueElement);
-    
-    this.listElement = document.createElement('ul');
-    this.listElement.classList.add('component-dropdown-list');
-    this.element.append(this.listElement);
+    constructor(defaultValue, listItems, onSelectItem) {
+        this.defaultValue = defaultValue;
+        this._selectedValue = defaultValue;
+        this.listItems = listItems;
 
-    this.listItems.forEach(value => {
-      const li = document.createElement('li');
-      li.innerText = value;
-      li.classList.add('component-dropdown-item');
-      this.listElement.append(li);
-    });
+        this.element = document.createElement("div");
+        this.element.classList.add("component-dropdown-container");
 
-    this.element.addEventListener('click', (event) => {
-      this.element.classList.toggle('active');
-      if (event.target.matches('.component-dropdown-value')) {
-        this.clearSelectedValue();
-        if(onSelectItem) onSelectItem();
-      }
-      if (event.target.matches('.component-dropdown-item')) {
-        this.selectedValue = event.target.innerText;
-        if(onSelectItem) onSelectItem(event.target.innerText);
-      };
-    })
-  }
+        this.valueElement = document.createElement("div");
+        this.valueElement.classList.add("component-dropdown-value");
+        this.valueElement.innerText = this.defaultValue;
+        this.element.append(this.valueElement);
 
-  set selectedValue(value) {
-    this.valueElement.innerText = value;
-    this._selectedValue = value;
-  }
+        this.listElement = document.createElement("ul");
+        this.listElement.classList.add("component-dropdown-list");
+        this.element.append(this.listElement);
 
-  get selectedValue() {
-    return this._selectedValue === this.defaultValue ? null : this._selectedValue;
-  }
+        // this.listItems.forEach((value) => {
 
-  clearSelectedValue() {
-    this.selectedValue = this.defaultValue;
-  }
-  
-  setItemsList(list) {
-    this.listElement.innerHTML = '';
-    list.forEach(value => {
-      const li = document.createElement('li');
-      li.innerText = value;
-      li.classList.add('component-dropdown-item');
-      this.listElement.append(li);
-    });
-  }
+        // });
+
+        this.element.addEventListener("click", (event) => {
+            this.element.classList.toggle("active");
+            if (event.target.matches(".component-dropdown-value")) {
+                this.clearSelectedValue();
+                if (onSelectItem) onSelectItem();
+            }
+            if (event.target.matches(".component-dropdown-item")) {
+                this.selectedValue = event.target.innerText;
+                if (onSelectItem) onSelectItem(event.target.innerText);
+            }
+        });
+    }
+
+    set selectedValue(value) {
+        this.valueElement.innerText = value;
+        this._selectedValue = value;
+    }
+
+    get selectedValue() {
+        return this._selectedValue === this.defaultValue ?
+            null :
+            this._selectedValue;
+    }
+
+    clearSelectedValue() {
+        this.selectedValue = this.defaultValue;
+    }
+
+    setItemsList(list) {
+        this.listElement.innerHTML = "";
+        list.forEach((value) => {
+            const li = document.createElement("li");
+            li.innerText = value;
+            li.classList.add("component-dropdown-item");
+            this.listElement.append(li);
+        });
+    }
+    getListBrands(response) {
+        response.forEach((value) => {
+            const li = document.createElement("li");
+            li.innerText = value;
+            li.classList.add("component-dropdown-item");
+            this.listElement.append(li);
+        });
+    }
 }
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import { brands } from "./constants/cars.js";
 import Dropdown from "./classes/Dropdown.js";
 import Card from "./classes/Card.js";
 import Router from "./classes/Router.js";
@@ -16,70 +15,93 @@ const router = new Router(routerContainer);
 
 // Фильтры
 const modelDropdown = new Dropdown("Выберите модель", []);
-const brandDropdown = new Dropdown("Выберите марку", brands, (brand) => {
-  modelDropdown.setItemsList([]);
-  modelDropdown.clearSelectedValue();
-  if (brand) {
-    getCars().then((cars) => {
-      let models = cars
-        .filter((car) => car.brand === brand)
-        .map((car) => car.model);
-      models = Array.from(new Set(models));
-      modelDropdown.setItemsList(models);
-    });
-  }
-});
+
+const brandDropdown = new Dropdown(
+    "Выберите марку",
+    getBrands().then((response) => {
+        // обращаемся к методу класса со своим массивом сервака и обрабатываем его
+        brandDropdown.getListBrands(response);
+    }),
+    (brand) => {
+        modelDropdown.setItemsList([]);
+        modelDropdown.clearSelectedValue();
+
+        if (brand) {
+            getModels(brand).then((cars) => {
+                let models = cars;
+
+                modelDropdown.setItemsList(models);
+            });
+        }
+    }
+);
+
 filtersContainer.append(brandDropdown.element);
 filtersContainer.append(modelDropdown.element);
 
 getCars().then((cars) => {
-  Card.appendCards(cardsContainer, cars, (carId) => {
-    router.goTo("#car");
-    getCar(carId).then((car) => {
-      BigCard.appendCard(bigCardContainer, car);
+    Card.appendCards(cardsContainer, cars, (carId) => {
+        router.goTo("#car");
+        getCar(carId).then((car) => {
+            BigCard.appendCard(bigCardContainer, car);
+        });
     });
-  });
 });
 
 searchButton.onclick = () => {
-  cardsContainer.innerHTML = "";
-  getCars().then((cars) => {
-    const filteredCars = cars.filter((car) => {
-      const isBrand = !!brandDropdown.selectedValue;
-      const isModel = !!modelDropdown.selectedValue;
-      if (isBrand && isModel) {
-        return (
-          car.brand === brandDropdown.selectedValue &&
-          car.model === modelDropdown.selectedValue
-        );
-      } else if (isBrand && !isModel) {
-        return car.brand === brandDropdown.selectedValue;
-      } else {
-        return true;
-      }
+    cardsContainer.innerHTML = "";
+    getCars().then((cars) => {
+        const filteredCars = cars.filter((car) => {
+            const isBrand = !!brandDropdown.selectedValue;
+            const isModel = !!modelDropdown.selectedValue;
+            if (isBrand && isModel) {
+                return (
+                    car.brand === brandDropdown.selectedValue &&
+                    car.model === modelDropdown.selectedValue
+                );
+            } else if (isBrand && !isModel) {
+                return car.brand === brandDropdown.selectedValue;
+            } else {
+                return true;
+            }
+        });
+        Card.appendCards(cardsContainer, filteredCars, (carId) => {
+            router.goTo("#car");
+            getCar(carId).then((car) => {
+                BigCard.appendCard(bigCardContainer, car);
+            });
+        });
     });
-    Card.appendCards(cardsContainer, filteredCars, (carId) => {
-      router.goTo("#car");
-      getCar(carId).then((car) => {
-        BigCard.appendCard(bigCardContainer, car);
-      });
-    });
-  });
 };
 
 // Запросы к серверу
 function getCars() {
-  // Получить все авто
-  return fetch("https://cars-server.herokuapp.com/cars").then((response) => {
-    return response.json();
-  });
+    // Получить все авто
+    return fetch("https://cars-server.herokuapp.com/cars").then((response) => {
+        return response.json();
+    });
 }
 
 function getCar(id) {
-  // Получить авто по id
-  return fetch(`https://cars-server.herokuapp.com/cars/${id}`).then(
-    (response) => {
-      return response.json();
-    }
-  );
+    // Получить авто по id
+    return fetch(`https://cars-server.herokuapp.com/cars/${id}`).then(
+        (response) => {
+            return response.json();
+        }
+    );
+}
+// Получаем бренды через фетч
+function getBrands() {
+    return fetch("https://cars-server.herokuapp.com/brands").then((response) => {
+        return response.json();
+    });
+}
+
+// Получаем модели
+function getModels(brand) {
+    return fetch(`https://cars-server.herokuapp.com/models/${brand}`).then(
+        (response) => {
+            return response.json();
+        }
+    );
 }
