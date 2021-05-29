fb5a6414bade69a841ea4d73e237be76e63e295e
No-Max <malyavko.max@gmail.com>
2021-05-29 10:02:08 +0300

    format poup-up

--- classes/PopUp.js
+++ classes/PopUp.js
@@ -1,21 +1,21 @@
 export default class PopUp {
-  constructor(delay = 1000, container = document.body) {
-    this.element = document.createElement('div');
+  constructor(delay = 1800, container = document.body) {
+    this.element = document.createElement("div");
     this.delay = delay;
 
-    this.element.className = 'pop-up';
+    this.element.className = "pop-up";
     container.append(this.element);
 
-    document.addEventListener('pop-up', e => {
-      this.pushMessage(e.detail.message)
-    })
+    document.addEventListener("pop-up", (e) => {
+      this.pushMessage(e.detail.message);
+    });
   }
 
   pushMessage(message) {
-    this.element.classList.add('active');
-    this.element.innerText = message || '';
+    this.element.classList.add("active");
+    this.element.innerText = message || "";
     const timer = setTimeout(() => {
-      this.element.classList.remove('active');
+      this.element.classList.remove("active");
       clearTimeout(timer);
     }, this.delay);
   }
