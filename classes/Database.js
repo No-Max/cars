3e250d99bbabf4d10cdad1060a2b7b536d483ee9
4ebula <4ebula@gmail.com>
2021-06-04 15:30:15 +0300

    feat: add upload event on form

--- classes/Database.js
+++ classes/Database.js
@@ -1,16 +1,28 @@
 import { getBrands } from "../services/brands.js";
 import { getModels } from "../services/models.js";
+import { upload } from "../services/upload.js";
 
 export class Database {
   element;
   brand;
   model;
+  result;
   constructor(element) {
     this.element = element;
     this.button = this.element[this.element.length - 1];
     this.button.onclick = (event) => {
       event.preventDefault();
-      // validate() && send();
+      if (this.validate()) {
+        !!this.query && upload(this.query);
+        console.log(this.query);
+        this.query = null;
+        this.brand.selectedIndex = 0;
+        this.clearSelect.call(this.model, 'model', true);
+        Array.from(this.element).slice(2, -1).forEach((input) => {
+          input.value = '';
+        });
+
+      }
     };
 
     this.brand = this.element.brand;
@@ -30,7 +42,7 @@ export class Database {
   }
 
   async populateModels(id) {
-    this.populateSelect.call(this.model, getModels, 'model', this.clearSelect, id);
+    await this.populateSelect.call(this.model, getModels, 'model', this.clearSelect, id);
     this.model.disabled = false;
   }
 
@@ -48,5 +60,25 @@ export class Database {
     this.innerHTML = '';
     this.add(new Option(`Select ${name}`, '0'));
     this.disabled = disable || false;
+  }
+
+  validate() {
+    const inputs = Array.from(this.element).slice(0, -1).map((input) => input.value);
+    const isValidInputs = inputs.slice(2).every((input) => input.value !== '')
+
+    if (!this.brand.selectedIndex || !this.model.selectedIndex) return false;
+    if (!isValidInputs) return false;
+
+    const [BrandId, ModelId, img, bigImg, price, description] = inputs;
+    const car = {
+      BrandId,
+      ModelId,
+      img,
+      bigImg,
+      price,
+      description,
+    }
+    this.query = JSON.stringify(car);
+    return true;
   }
 }
